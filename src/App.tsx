8a337d118ead523af0808a9f6c2f0ea3f0422b46
JoshK7 <joshk7683@gmail.com>
2025-01-06 22:15:15 -0600

    wrapped sections within the main tag

--- src/App.tsx
+++ src/App.tsx
@@ -16,98 +16,100 @@ const App = () => {
                     />
                 </nav>
             </header>
-            <section className="hero">
-                <picture className="hero__banner">
-                    <img
-                        className="hero__img"
-                        src="/assets/tablet/image-hero.png"
-                        alt="Banner"
-                    />
-                </picture>
-                <article className="hero__container">
-                    <picture className="hero__left">
+            <main>
+                <section className="hero">
+                    <picture className="hero__banner">
                         <img
-                            className="hero__left--img"
-                            src="/assets/desktop/image-hero-left.png"
-                            alt="Banner Left"
+                            className="hero__img"
+                            src="/assets/tablet/image-hero.png"
+                            alt="Banner"
                         />
                     </picture>
-                    <div className="hero__text">
-                        <h1 className="hero__text--title">
-                            Group Chat <br />
-                            for Everyone
-                        </h1>
-                        <p className="hero__text--description">
-                            Meet makes it easy to connect with others
-                            face-to-face virtually and collaborate across any
-                            device.
-                        </p>
-                        <div className="hero__buttons">
-                            <Button color="cyan">
-                                Download <span>v1.3</span>
-                            </Button>
-                            <Button>What is it?</Button>
+                    <div className="hero__container">
+                        <picture className="hero__left">
+                            <img
+                                className="hero__left--img"
+                                src="/assets/desktop/image-hero-left.png"
+                                alt="Banner Left"
+                            />
+                        </picture>
+                        <div className="hero__text">
+                            <h1 className="hero__text--title">
+                                Group Chat <br />
+                                for Everyone
+                            </h1>
+                            <p className="hero__text--description">
+                                Meet makes it easy to connect with others
+                                face-to-face virtually and collaborate across
+                                any device.
+                            </p>
+                            <div className="hero__buttons">
+                                <Button color="cyan">
+                                    Download <span>v1.3</span>
+                                </Button>
+                                <Button>What is it?</Button>
+                            </div>
+                        </div>
+                        <picture className="hero__right">
+                            <img
+                                className="hero__right--img"
+                                src="/assets/desktop/image-hero-right.png"
+                                alt="Banner Right"
+                            />
+                        </picture>
+                    </div>
+                </section>
+                <section className="center">
+                    <div className="center__container">
+                        <Number>01</Number>
+                        <ul className="center__list">
+                            <li className="center__list--item">
+                                <img
+                                    className="center__list--img"
+                                    src="/assets/desktop/image-woman-in-videocall.jpg"
+                                    alt="Woman in video call"
+                                />
+                            </li>
+                            <li className="center__list--item">
+                                <img
+                                    className="center__list--img"
+                                    src="/assets/desktop/image-women-videochatting.jpg"
+                                    alt="Women video chatting"
+                                />
+                            </li>
+                            <li className="center__list--item">
+                                <img
+                                    className="center__list--img"
+                                    src="/assets/desktop/image-men-in-meeting.jpg"
+                                    alt="Men in meeting"
+                                />
+                            </li>
+                            <li className="center__list--item">
+                                <img
+                                    className="center__list--img"
+                                    src="/assets/desktop/image-man-texting.jpg"
+                                    alt="Man texting"
+                                />
+                            </li>
+                        </ul>
+                        <div className="center__text">
+                            <div className="center__heading">
+                                <span className="center__heading--span">
+                                    Built for modern use
+                                </span>
+                                <h2 className="center__heading--title">
+                                    Smarter meetings, all in one place
+                                </h2>
+                            </div>
+                            <p className="center__description">
+                                Send messages, share files, show your screen,
+                                and record your meetings — all in one workspace.
+                                Control who can join with invite-only team
+                                access, data encryption, and data export.
+                            </p>
                         </div>
                     </div>
-                    <picture className="hero__right">
-                        <img
-                            className="hero__right--img"
-                            src="/assets/desktop/image-hero-right.png"
-                            alt="Banner Right"
-                        />
-                    </picture>
-                </article>
-            </section>
-            <main className="center">
-                <div className="center__container">
-                    <Number>01</Number>
-                    <ul className="center__list">
-                        <li className="center__list--item">
-                            <img
-                                className="center__list--img"
-                                src="/assets/desktop/image-woman-in-videocall.jpg"
-                                alt="Woman in video call"
-                            />
-                        </li>
-                        <li className="center__list--item">
-                            <img
-                                className="center__list--img"
-                                src="/assets/desktop/image-women-videochatting.jpg"
-                                alt="Women video chatting"
-                            />
-                        </li>
-                        <li className="center__list--item">
-                            <img
-                                className="center__list--img"
-                                src="/assets/desktop/image-men-in-meeting.jpg"
-                                alt="Men in meeting"
-                            />
-                        </li>
-                        <li className="center__list--item">
-                            <img
-                                className="center__list--img"
-                                src="/assets/desktop/image-man-texting.jpg"
-                                alt="Man texting"
-                            />
-                        </li>
-                    </ul>
-                    <div className="center__text">
-                        <div className="center__heading">
-                            <span className="center__heading--span">
-                                Built for modern use
-                            </span>
-                            <h2 className="center__heading--title">
-                                Smarter meetings, all in one place
-                            </h2>
-                        </div>
-                        <p className="center__description">
-                            Send messages, share files, show your screen, and
-                            record your meetings — all in one workspace. Control
-                            who can join with invite-only team access, data
-                            encryption, and data export.
-                        </p>
-                    </div>
-                </div>
+                </section>
             </main>
             <footer>
                 <div className="footer__overlay">
